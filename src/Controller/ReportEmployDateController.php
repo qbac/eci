96a8beca291262dd9df06a4b377b7825f76c13cf
qbac <jakub.cyrklaf@gmail.com>
2022-05-18 10:47:26 +0200

    add report pdf in reportEmployDate

--- src/Controller/ReportEmployDateController.php
+++ src/Controller/ReportEmployDateController.php
@@ -9,7 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Dompdf\Dompdf;
+use Dompdf\Options;
 class ReportEmployDateController extends AbstractController
 {
     #[Route('/report/employ/date', name: 'app_report_employ_date')]
@@ -26,6 +27,7 @@ class ReportEmployDateController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid())
         {
+            $nextAction = $form->get('pdf')->isClicked() ? 'genPdf': 'preview';
             $visibleResult = true;
             $idEmploy = $workTime->getEmploy()->getId();
             $dateStart = $form->get('work_date_start')->getData()->format('Y-m-d');
@@ -33,6 +35,11 @@ class ReportEmployDateController extends AbstractController
             $resultSum = $workTimeRepository->getEmployDataWorkTimeSum($idEmploy, $dateStart, $dateEnd);
             $resultDays = $workTimeRepository->getEmployDataWorkTime($idEmploy, $dateStart, $dateEnd);
             $resultTotal = $workTimeRepository->getEmployDataTotalSum($idEmploy, $dateStart, $dateEnd);
+
+            if ($nextAction == 'genPdf') {
+                $employName = $workTime->getEmploy()->getName();
+                return $this->pdfReport($idEmploy, $dateStart, $dateEnd, $employName, $workTimeRepository);
+            }
         }
 
         return $this->render('report_employ_date/index.html.twig', [
@@ -43,5 +50,40 @@ class ReportEmployDateController extends AbstractController
             'resultReportDays' => $resultDays,
             'resultTotal' => $resultTotal
         ]);
+    }
+
+    #[Route('/report/employ/pdf', name: 'app_report_employ_pdf')]
+    public function pdfReport($idEmploy, $dateStart, $dateEnd, $employName, WorkTimeRepository $workTimeRepository)
+    {
+        $resultSum = $workTimeRepository->getEmployDataWorkTimeSum($idEmploy, $dateStart, $dateEnd);
+        $resultDays = $workTimeRepository->getEmployDataWorkTime($idEmploy, $dateStart, $dateEnd);
+        $resultTotal = $workTimeRepository->getEmployDataTotalSum($idEmploy, $dateStart, $dateEnd);
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Courier');
+        $pdfOptions->set('isPhpEnabled', true);
+        
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('report_employ_date/pdf.html.twig', [
+            'title' => "Raport",
+            'resultReportSum' => $resultSum,
+            'resultReportDays' => $resultDays,
+            'resultTotal' => $resultTotal,
+            'dateStart' => $dateStart,
+            'dateEnd' => $dateEnd,
+            'employName' => $employName,
+        ]);
+        
+        $filename = 'raport-emp_'.$employName.'_'.$dateStart.'_'.$dateEnd.'.pdf';
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $dompdf->stream($filename, [
+            "Attachment" => true
+        ]);
+
+        //return $this->redirectToRoute('app_report_user_date');
     }
 }
